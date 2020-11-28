4dfa4dd681da45688840032055bf2b766be59233
ozonet <ozonet@kali.ozonet>
2020-11-28 17:27:09 +0000

    Split parse and core, add some fixes, make flags a bit intuitive

--- pass_generator.py
+++ pass_generator.py
@@ -40,14 +40,14 @@ def create_shuffle_ld(digits, letters, symbols, upper, lower):
     return string_letters, string_else
 
 
-def password_generator(length=16, digits=True, letters=True, symbols=True, upper=False, lower=False, ld=False, uniq=False):
+def password_generator(length=16, digits=True, letters=True, symbols=True, upper=False,
+                       lower=False, ld=False, uniq=False):
     random_password = ''
     if ld:
         printable_letters, printable_else = create_shuffle_ld(digits, letters, symbols, upper, lower)
 
-        if uniq:
-            printable_letters = ''.join(c for c in printable_letters if c not in BANNED)
-            printable_else = ''.join(c for c in printable_else if c not in BANNED)
+        printable_letters = ''.join(c for c in printable_letters if c not in BANNED) if uniq else printable_letters
+        printable_else = ''.join(c for c in printable_else if c not in BANNED) if uniq else printable_else
 
         printable_letters = list(printable_letters)
         printable_else = list(printable_else)
@@ -57,19 +57,20 @@ def password_generator(length=16, digits=True, letters=True, symbols=True, upper
 
         random_number = random.randint(1, length)
         last_number = length - random_number
+
         random_password_part_one = random.choices(printable_letters, k=random_number)
         random_password_part_two = random.choices(printable_else, k=last_number)
 
         random_password = ''.join(random_password_part_one) + ''.join(random_password_part_two)
+
     else:
         printable = create_shuffle(digits, letters, symbols, upper, lower)
-
-        if uniq:
-            printable = ''.join(c for c in printable if c not in BANNED)
+        printable = ''.join(c for c in printable if c not in BANNED) if uniq else printable
 
         printable = list(printable)
         random.shuffle(printable)
         random_password = random.choices(printable, k=length)
+
         random_password = ''.join(random_password)
     return random_password
 
@@ -148,7 +149,7 @@ Good luck!
 """
 
 
-def main_core():
+def parser():
     parse = OptionParser(bcolors.HEADER + art + help_menu + bcolors.ENDC)
 
     parse.add_option("-l", "--len", dest="Length", type="string", help="Specify password length")
@@ -158,26 +159,35 @@ def main_core():
                      help="Specify the username (optional for file output)")
     parse.add_option("-o", "--out", dest="File", type="string",
                      help="Output file. If it exists it will be overwrited!")
-    parse.add_option("-s", dest="S", action='store_false', help="Do not use punctuation symbols")
-    parse.add_option("-c", dest="C", action='store_false', help="Do not use letters")
-    parse.add_option("-d", dest="D", action='store_false', help="Do not use digits")
+    parse.add_option("-P", dest="P", action='store_false', help="Do not use punctuation symbols")
+    parse.add_option("-L", dest="L", action='store_false', help="Do not use letters")
+    parse.add_option("-D", dest="D", action='store_false', help="Do not use digits")
     parse.add_option("-A", dest="A", action='store_true', help="The first letter of the password is capitalized")
-    parse.add_option("-U", dest="Similar", action='store_true', help="Exclude Similar Characters (i, I, l, 1, o, O, 0")
-    parse.add_option("--uc", dest="Upper", action='store_false', help="Use only upper case letters")
-    parse.add_option("--lc", dest="Lower", action='store_false', help="Use only lower case letters")
-    parse.add_option("--ld", dest="LD", action='store_false', help="Letters first")
+    parse.add_option("-U", dest="Similar", action='store_true', help="Exclude Similar Characters")
+    parse.add_option("--uce", dest="Upper", action='store_false', help="Use only upper case letters")
+    parse.add_option("--lce", dest="Lower", action='store_false', help="Use only lower case letters")
+    parse.add_option("--lf", dest="LD", action='store_false', help="Letters first")
     parse.add_option("--base64", dest="BASE64", action='store_true', help="Encrypt your password with base64")
     parse.add_option("--md5", dest="MD5", action='store_true', help="Encrypt your password with MD5")
-    (opt, args) = parse.parse_args()
+    return parse.parse_args()
 
-    if opt.Length is None and opt.URL is None and opt.Username is None and opt.File is None and opt.Upper is None \
-            and opt.BASE64 is None and opt.S is None and opt.C is None and opt.D is None and opt.Similar is None\
-            and opt.Lower is None and opt.LD is None and opt.MD5 is None and opt.A is None:
+
+def opt_is_none(opt):
+    return opt.Length is None and opt.URL is None and opt.Username is None and opt.File is None and opt.Upper is None \
+           and opt.BASE64 is None and opt.P is None and opt.L is None and opt.D is None and opt.Similar is None \
+           and opt.Lower is None and opt.LD is None and opt.MD5 is None and opt.A is None
+
+
+def main_core():
+    # Parse arguments
+    (opt, args) = parser()
+
+    # If program called without arguments
+    if opt_is_none(opt):
         password = password_generator()
         print(printer(password))
         exit(0)
 
-    string = ''
     LENGTH = 16
     try:
         LENGTH = int(opt.Length) if opt.Length is not None else 16
@@ -185,9 +195,9 @@ def main_core():
         print(f'{bcolors.HEADER}{art}{bcolors.ENDC}Error error, as always! Type  -h  for help\n\n')
         exit(0)
 
-    PUNCTUATION_SYMBOLS = True if opt.S is None else False
     DIGITS = True if opt.D is None else False
-    LETTER_CHARS = True if opt.C is None else False
+    LETTER_CHARS = True if opt.L is None else False
+    PUNCTUATION_SYMBOLS = True if opt.P is None else False
 
     if not PUNCTUATION_SYMBOLS and not DIGITS and not LETTER_CHARS:
         print(f'{bcolors.HEADER}{art}Sorry, you disabled everything, here is you password:\n\n'
@@ -207,18 +217,11 @@ def main_core():
                                 upper=UPPER_FLAG, lower=LOWER_FLAG, ld=LETTERS_FIRST, uniq=UNIQ_CHARS)
 
     # Capitalize
-    if opt.A:
-        string = string.capitalize()
+    string = string.capitalize() if opt.A else string
 
     # Encryption
-    b64_string = ''
-    md5_string = ''
-
-    if opt.BASE64:
-        b64_string = base64.b64encode(string.encode()).decode()
-
-    if opt.MD5:
-        md5_string = hashlib.md5(string.encode()).hexdigest()
+    b64_string = base64.b64encode(string.encode()).decode() if opt.BASE64 else ''
+    md5_string = hashlib.md5(string.encode()).hexdigest() if opt.MD5 else ''
 
     # if password will be written into file
     if opt.File is not None:
@@ -228,9 +231,11 @@ def main_core():
         save_to_file(string, FILEPATH, username=USERNAME, url=URL, b64_string=b64_string, md5_string=md5_string)
         exit(0)
 
+    # Main print
     print(printer(string, b64_string=b64_string, md5_string=md5_string))
     exit(0)
 
 
 if __name__ == "__main__":
     main_core()
+    exit(0)
